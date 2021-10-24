3f56b85ef1373e458130f8f8c36d8764b88568d3
StevenJiao <syjiao@ualberta.ca>
2021-10-24 01:32:01 -0600

    added some starter stuff to prodcon

--- assignments/assignment2/prodcon.cpp
+++ assignments/assignment2/prodcon.cpp
@@ -6,12 +6,21 @@ using namespace std;
 
 int main(int argc, char const *argv[]) {
     queue<string> myQ;
-    int test;
+
+    // get the input either from commandline or file
     string line;
     while (getline(cin, line)) {
+        // add to our queue
         myQ.push(line);
     }
-    cout << argv[1] << endl;
-    cout << "Number of args: " << argc << endl;
+
+    // get the number of threads and output file number if given
+    int numThreads = *argv[1];
+    int maxQSize = 2*numThreads;
+    string outputFileNum = argc == 3 ? argv[2] : "";
+    string outputFileName = "prodcon." + outputFileNum + ".log";
+
+    // spawn our threads
+    cout << outputFileName << endl;
     return 0;
 }
